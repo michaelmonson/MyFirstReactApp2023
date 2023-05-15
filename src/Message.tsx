955d27e52b6902b6e867c0d0d282d765b7b417a8
Michael Monson <michael.monson@stgconsulting.com>
2023-05-14 23:07:24 -0600

    My initial commit of local files

---
+++ src/Message.tsx
@@ -0,0 +1,5 @@
+//Pascal Casing (convention used by REACT and most front-end/stack developers using REACT)
+function Message() {
+    //JSX: JavaScript XML
+    return <h1>Hello weary traveler, to this glorious World!</h1>
+}
