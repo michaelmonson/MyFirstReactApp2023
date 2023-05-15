fc77ce6d97efa198c99e914296e6bb8d40e165b4
Michael Monson <michael.monson@stgconsulting.com>
2023-05-15 13:11:37 -0600

    Making the Message dynamic (sort of) by name   - Also adding comments where appropriate.

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,6 +1,14 @@
 //Pascal Casing (convention used by REACT and most front-end/stack developers using REACT)
 function Message() {
     //JSX: JavaScript XML
+    var visitorName = 'Sariah';
+    //visitorName = 'Michael';
+    //visitorName = '';
+
+    if (visitorName) {
+        return <h1>Hello {visitorName}, to this great place!</h1>;    
+    }
+    //returns value when visitorName has not been set, or if it is empty:
     return <h1>Hello weary traveler, to this glorious, amazing, natural World!</h1>;
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,24 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+/* 
+ * This ReactDOM.createRoot(...) is where some of the REACT magic occurs!
+ * The 'createRoot' function above looks for the DOM element called 'root' which is found in our index.html file
+ */
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(  
+  //Component Tree : It wraps our 'App.tsx' component.  It is a 'built-in' component of React.  Purpose is to find potential problems.
   <React.StrictMode>
     <App />
   </React.StrictMode>,
 )
+
+/**
+ * NOTE: With the code above, we're taking this component tree and rendering it (displaying) 
+ * inside an element with the ID of 'root' and using the ReactDOM library. 
+ * 
+ * It is important to understand that we could alternatively render this component Tree in a mobile app 
+ * using a different Library called 'ReactNative.'
+ * 
+ * So REACT itself is not tied to a particular platform like web or mobile... it's platform agnostic
+ * and we can use it to build apps for web mobile and desktop devices.
+ */
