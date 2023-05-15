ec4b38b843501a31c963cdd1f58db10f8234ac51
Michael Monson <michael.monson@stgconsulting.com>
2023-05-15 10:58:17 -0600

    Building my first React Component!  Whoo Hoo!   - Zapping away the default 'ViteJS' component (typescript)   - Replacing the App.tsx code with my own REACT component.

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,7 @@
 //Pascal Casing (convention used by REACT and most front-end/stack developers using REACT)
 function Message() {
     //JSX: JavaScript XML
-    return <h1>Hello weary traveler, to this glorious World!</h1>
+    return <h1>Hello weary traveler, to this glorious, amazing, natural World!</h1>;
 }
+
+export default Message;
