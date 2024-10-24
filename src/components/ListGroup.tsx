42c048fbba373daf8e05cb52a0f5a8003647ed31
Michael Monson <michaelmonson@gmail.com>
2024-10-23 23:41:00 -0600

    Adding logic for when items are not found in list

--- src/components/ListGroup.tsx
+++ src/components/ListGroup.tsx
@@ -9,6 +9,14 @@ function ListGroup() {
     "Riverton",
   ];
 
+  if (items.length === 0)
+    return (
+      <>
+        <h1>List</h1>
+        <p>No items found...</p>
+      </>
+    );
+
   return (
     //Using the angle bracket notation (open and close) to replace the 'Fragment' component
     <>
